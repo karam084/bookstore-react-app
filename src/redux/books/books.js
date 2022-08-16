a44a7c25d2be11d57684b0c1fa9fb790703f6dde
karam084 <karam.ezz84@gmail.com>
2022-08-17 00:52:06 +0200

    Add functionality add and remove book

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,37 @@
-const ADDBOOK = 'bookstore/books/ADDBOOK';
-const REMOVEBOOOK = 'bookstore/books/REMOVEBOOK';
+const ADDBOOK = 'bookstore-react-ap/books/ADDBOOK';
+const REMOVEBOOOK = 'bookstore-react-ap/books/REMOVEBOOK';
 
+const booksReducer = (state = [], action) => {
+    switch (action.type) {
+        // do reducer stuff
+        case 'ADDBOOK':
+            return [state, {
+                id: action.id,
+                title: action.title,
+                author: action.author,
+            }];
+        case 'REMOVEBOOOK':
+            return state.filter((book) => book.id !== action.id);
+
+        default: return state;
+    }
+}
+export const addBook = (bookTitle, bookAuthor, bookId) => {
+    const book = {
+        type: ADDBOOK,
+        title: bookTitle,
+        author: bookAuthor,
+        id: bookId,
+    };
+    return book;
+};
+
+export const removeBook = (bookId) => {
+    const removedBook = {
+        type: REMOVEBOOOK,
+        id: bookId,
+    };
+    return removedBook;
+};
+
+export default booksReducer;
