ee9244ba3561cccfb4fa497edaa42e3e1893990e
karam084 <karam.ezz84@gmail.com>
2022-08-17 02:55:00 +0200

    Fix linters

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,31 +1,28 @@
 const ADDBOOK = 'bookstore-react-ap/books/ADDBOOK';
 const REMOVEBOOOK = 'bookstore-react-ap/books/REMOVEBOOK';
 
-const booksReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADDBOOK':
-            return [
-                state,
-                action.payload,
-            ];
-        case 'REMOVEBOOOK':
-            return state.filter((book) => book.id !== action.id);
-
-        default: return state;
-    }
-}
 export const addBook = (book) => {
-    const book = {
+    let book = {
         type: ADDBOOK,
         payload: book
     };
     return book;
 };
+const booksReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADDBOOK':
+            return [state, action.payload];
+        case 'REMOVEBOOOK':
+            return state.filter((book) => book.id !== action.id);
 
+        default:
+            return state;
+    }
+};
 export const removeBook = (bookId) => {
     const removedBook = {
         type: REMOVEBOOOK,
-        playLoad: bookId,
+        payload: bookId,
     };
     return removedBook;
 };
