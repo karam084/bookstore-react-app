67ee0a637fa8dfa572a1ea26f6366a4c50789efa
karam084 <karam.ezz84@gmail.com>
2022-08-19 02:28:22 +0200

    Add dispatch to form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,21 @@
-const Form = () => (
+
+import { useDispatch } from 'react-redux/es/exports';
+import React, { useRef } from 'react';
+import uniqid from 'uniqid';
+import { addBook } from '../redux/books/books';
+
+const Form = () => {
+  const dispatch = useDispatch();
+  const title = useRef(null);
+  const author = useRef(null);
   <>
     <div><label className="title">ADD NEW BOOK</label></div>
     <form className="form">
-      <input type="text" name="book" placeholder="Book" className="book" />
-      <input type="text" name="author" placeholder="Author" className="author" />
-      <button type="submit" value="Add" className="submit">Add Book</button>
+      <input ref={title} type="text" name="book" placeholder="Book" className="book" />
+      <input ref={author} type="text" name="author" placeholder="Author" className="author" />
+      <button onClick={() => dispatch(addBook(title.current.value, author.current.value, uniqid()))} typeaut="submit" value="Add" className="submit">Add Book</button>
     </form>
   </>
-);
+};
 
 export default Form;
