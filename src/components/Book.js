bfe479d0381c444829113f98c3e7f0c7110da3e9
karam084 <karam.ezz84@gmail.com>
2022-08-19 02:15:26 +0200

    Add useSelector and dispatch action

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,53 +1,64 @@
-const Book = () => (
-  <>
-    <div className="card">
-      <div className="title">
-        <span>Action</span>
-        <h1>The hunger games</h1>
-        <p>Unseen Content</p>
-        <div className="action-buttons">
-          <button className="button-outline" type="button">Comments</button>
-          <div className="vertical-divider" />
-          <button className="button-outline" type="button">Remove</button>
-          <div className="vertical-divider" />
-          <button className="button-outline" type="button">Edit</button>
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook, booksReducer } from '../redux/books/books';
+
+
+const Book = () => {
+  const books = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
+  return (books.map((book) => (
+    <>
+      <div key={book.id} className="card">
+        <div className="title">
+          <span>Action</span>
+          <h1>{book.title}</h1>
+          <p>{book.author}</p>
+          <div className="action-buttons">
+            <button className="button-outline" type="button">Comments</button>
+            <div className="vertical-divider" />
+            <button onClick={(e) => {
+              e.preventDefault();
+              dispatch(removeBook(book.id));
+            }} className="button-outline" type="button">Remove</button>
+            <div className="vertical-divider" />
+            <button className="button-outline" type="button">Edit</button>
+          </div>
+        </div>
+
+        <div className="progress">
+          <h4>60%</h4>
+        </div>
+        <div className="chapter">
+          <h4>Current Chapter</h4>
+          <h5>Chapter 16</h5>
+          <button type="button">UPDATE PROGRESS</button>
         </div>
       </div>
 
-      <div className="progress">
-        <h4>60%</h4>
-      </div>
-      <div className="chapter">
-        <h4>Current Chapter</h4>
-        <h5>Chapter 16</h5>
-        <button type="button">UPDATE PROGRESS</button>
-      </div>
-    </div>
+      <div className="card">
+        <div className="title">
+          <span>Action</span>
+          <h1>The hunger games</h1>
+          <p>Unseen Content</p>
+          <div className="action-buttons">
+            <button className="button-outline" type="button">Comments</button>
+            <div className="vertical-divider" />
+            <button className="button-outline" type="button">Remove</button>
+            <div className="vertical-divider" />
+            <button className="button-outline" type="button">Edit</button>
+          </div>
+        </div>
 
-    <div className="card">
-      <div className="title">
-        <span>Action</span>
-        <h1>The hunger games</h1>
-        <p>Unseen Content</p>
-        <div className="action-buttons">
-          <button className="button-outline" type="button">Comments</button>
-          <div className="vertical-divider" />
-          <button className="button-outline" type="button">Remove</button>
-          <div className="vertical-divider" />
-          <button className="button-outline" type="button">Edit</button>
+        <div className="progress">
+          <h4>60%</h4>
+        </div>
+        <div className="chapter">
+          <h4>Current Chapter</h4>
+          <h5>Chapter 16</h5>
+          <button type="button">UPDATE PROGRESS</button>
         </div>
       </div>
-
-      <div className="progress">
-        <h4>60%</h4>
-      </div>
-      <div className="chapter">
-        <h4>Current Chapter</h4>
-        <h5>Chapter 16</h5>
-        <button type="button">UPDATE PROGRESS</button>
-      </div>
-    </div>
-  </>
-);
+    </>
+  )))
+};
 
 export default Book;
